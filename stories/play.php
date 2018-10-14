0dba9cfcdf79c55fcb2f067b0f7860816f5009a6
David Serr <dnetserr@gmail.com>
2018-10-13 23:06:36 -0400

    Improvements to the stories page

--- stories/play.php
+++ stories/play.php
@@ -1,17 +1,24 @@
+<!--
 <script>
-
+function swapWithPlayer(label){
+    var item = document.getElementById(label);
+    item.outerHTML = "<audio controls><source src=\"" + item.attributes.href.value + "\"/> </audio>"}
 </script>
+-->
+
 <?php
 $directory = $_GET["Folder"];
 $files = scandir( $directory  );
 foreach ( $files as $file) {
+    $path = $directory . '/' . $file;
     if (endsWith($file, "mp3")){
-        #echo "<br/><a href='${directory}/${file}' onclick='swapWithPlayer
-        echo "<br/><audio controls><source src='${directory}/${file}' type='audio/mpeg'/></audio>";
+        $label = substr($file, -10, 6);
+        echo "\n<br/><a id='${label}' href=\"${path}\"'>${label}</a>";
+        #echo "\n<br/>${label}<audio controls><source src=\"${path}\" type='audio/mpeg'/></audio>";
     } elseif (endsWith($file, "jpg")){
-        echo "<br/><img width='100' height='100' src='${directory}/${file}'/>";
+        echo "\n<br/><img width='100' height='100' src=\"${path}\"/>";
     } else {
-        echo "<!--<li>${directory}/${file}</li>-->";
+        echo "\n<!--${path}-->";
     }
 }
 
